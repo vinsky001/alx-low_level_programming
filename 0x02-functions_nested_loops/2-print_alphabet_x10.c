71f8d66e87fab53d7f13efede1be50e31feccb48
vinsky001 <ekibet544@gmail.com>
2022-12-13 04:53:58 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  *print_alphabet_x10 - function that prints alphabet x10
+  *Description: use the function print_alphabet_x10
+  *Return: 0
+  */
+void print_alphabet_x10(void)
+{
+	char b, j;
+
+	for (j = 0 ; j <= 9 ; j++)
+	{
+		for (b = 'a' ; b <= 'z' ; b++)
+		{
+			_putchar('b');
+		}
+		_putchar('\n');
+	}
+}
