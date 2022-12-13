291981bd69e65992f95f81543811f164795dbcb4
vinsky001 <ekibet544@gmail.com>
2022-12-13 08:14:10 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h" 
+#include <main.h>
 /**
   *main - print _putchar, followed by a new line
   *
@@ -18,5 +18,5 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 
-	return (0);
+	return(0);
 }
