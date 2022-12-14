b8a5c79b83a19e6c2509b29d80d45ae2199aab7d
vinsky001 <ekibet544@gmail.com>
2022-12-13 23:31:34 -0800

    Print the n times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+  *print_times_table - entry point
+  *@j: input
+  *Return: Number matrix
+  */
+
+void print_times_table(int j)
+{
+	int x, y, op;
+
+	if (j >= 0 && j <= 15)
+	{
+		for (x = 0; x <= j; x++)
+		{
+			_putchar(48);
+			for (y = 1; y <= j; y++)
+			{
+				op = x * y;
+                                _putchar(44);
+				_putchar(32);
+				if (op <= 9)
+			}
+			else if (op <= 99)
+			{
+				_putchar(32);
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+			else
+			{
+				_putchar(((op / 100) % 10) + 48);
+				_putchar(((op / 10) % 10) + 48);
+				_putchar((op % 10) + 48);
+			}
+		}
+		_putchar('\n');
+	}
+}
+	
