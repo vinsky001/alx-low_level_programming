bc6ed0d77cf3ca741cfe85e7a27dd0014c94ec9e
vinsky001 <ekibet544@gmail.com>
2022-12-15 07:09:45 -0800

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *print_triangle - print a triangle
+  *only use _putchar
+  *@size: is the triangle size
+  *if size is 0 or less, the function should print only a new line
+  *use the character # to print the triangle
+  *Return: success
+  */
+
+void print_triangle(int size)
+{
+
+		int i = 0, j, n = size - 1;
+
+		if (size > 0)
+		{
+			for (; i < size; i++)
+			{
+				for (j = 0; j < size; j++)
+				{
+					if (j < n)
+						_putchar(' ');
+					else
+						_putchar('#');
+				}
+				n--;
+				_putchar('\n');
+			}
+		}
+		else
+			_putchar('\n');
+}
+
+
