6bf60d5b1b79f617c58ff71d61ba5893a4b61b1d
vinsky001 <ekibet544@gmail.com>
2022-12-15 02:54:31 -0800

    print most numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,13 +10,13 @@
 
 void print_most_numbers(void)
 {
-		int i = '0';
+	int i = '0';
 
-		for (; i <= '9'; i++)
-		{
-			if (i != '2' && i != '4')
-				_putchar(i);
-
-		}
-		_putchar('\n');
+	for (; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
+	_putchar('\n');
 }
+
