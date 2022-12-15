d48867d02768d1624aa96216cf8a934ac352872b
vinsky001 <ekibet544@gmail.com>
2022-12-15 01:06:09 -0800

    Takes a date and prints how many days are left in the year

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,27 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
-#include "main.h"
 
-/**
-  *main - takes a date and prints how many days are left in the year,
-  *taking leap years into account
-  *Return: 0 (Success)
-  */
-int main(void)
-{
-	int month;
-	int day;
-	int year;
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
-	month = 2;
-	day = 29;
-	year = 2000;
-
-	printf("Date: %02d/%02d/%04d\n", month, day, year);
-
-	day = convert_day(month, day);
-
-	print_remaining_days(month, day, year);
-
-	return (0);
-}
-
+#endif /* MAIN_H */
