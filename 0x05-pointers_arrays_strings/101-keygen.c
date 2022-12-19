9e9b4d6a81c0e0990e85376627ec2d282f9dd773
vinsky001 <ekibet544@gmail.com>
2022-12-19 05:15:23 -0800

    random password generator

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+    *main - entry point
+    *Description: a program that generates random valid passwords
+    *for the program 101-crackme
+    *for the program 101-crackme
+    *Return: success
+*/
+int main(void)
+{
+	int myrand;
+	int count;
+	int total;
+
+	srand(time(NULL));
+	for (count = 0, total = 2772; total > 122; count++)
+	{
+		myrand = (rand() % 125) + 1;
+		printf("%c", myrand);
+		total -= myrand;
+	}
+	printf("%c", total);
+
+	return (0);
+}
+
