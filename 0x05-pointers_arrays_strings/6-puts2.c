8360b48a69ae35f17df2f7e5cb2e5a9e6ee0b315
vinsky001 <ekibet544@gmail.com>
2022-12-19 03:18:46 -0800

    prints characters of a string starting with the first

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+    *puts2 - prints every other character of a string,
+    *starting with the first character,
+    *followed by a new line
+    *@str: string pointer
+    *Return: void
+    */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			putchar(str[i]);
+		}
+		i++;
+	}
+	putchar('\n');
+}
