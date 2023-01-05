26d6db747bbcd0da9e940790d6d0c2c0f8fab035
vinsky001 <ekibet544@gmail.com>
2023-01-05 02:01:59 -0800

    Returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+    *_strlen_recursion - returns the length of a string
+    *@s: pointer to a string
+    *Return: success
+    */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+		return (0);
+}
