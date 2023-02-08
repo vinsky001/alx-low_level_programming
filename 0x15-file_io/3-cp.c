933c1412e9937943ddbcca322047b46c3f54bb75
vinsky001 <ekibet544@gmail.com>
2023-02-08 03:40:42 -0800

    A program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,6 @@
    * @argv: arguments vector.
    * Return: no return.
    */
-
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
@@ -42,6 +41,7 @@ int main(int argc, char *argv[])
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
+
 	nchars = 1024;
 	while (nchars == 1024)
 	{
@@ -52,12 +52,14 @@ int main(int argc, char *argv[])
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
+
 	err_close = close(file_from);
 	if (err_close == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
+
 	err_close = close(file_to);
 	if (err_close == -1)
 	{
