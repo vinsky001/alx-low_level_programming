d066e85bcaddff9d8db55dbab1ebce0c8b5d6c6e
vinsky001 <ekibet544@gmail.com>
2023-03-21 05:42:26 -0700

    Dynamic lib files

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+    *_memset - function that fills memory with a constant byte
+    *@b: the address of memory to print
+    *@s: the size of the memory to print
+    *@n: bytes of the memory
+    *Return: success
+    */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+	return (s);
+}
