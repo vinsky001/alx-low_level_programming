81483a9398c7fe9c3626b687432821653a961bbb
vinsky001 <ekibet544@gmail.com>
2023-04-08 12:22:02 -0700

    Hash_tables

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,9 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table.
+ * @ht: A pointer to the hash table to print.
+ *
+ * Description: Key/value pairs are printed in the order
+ * they appear in the array of the hash table.
+ */
